812b54dcff5239f914ad5fb43097219ca26da56e
jeremyc293 <jeremyc293@gmail.com>
2026-07-06 15:28:56 -0400

    About and Contact Page Functions

--- controllers.js
+++ controllers.js
@@ -2,3 +2,13 @@
 export const home = (req, res) => {
     res.send("Welcome Home!");
 };
+
+// About page
+export const about = (req, res) => {
+    res.send("About");
+};
+
+// Contact page
+export const contact = (req, res) => {
+    res.send("Contact");
+};
